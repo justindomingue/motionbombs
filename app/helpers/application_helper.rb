2839cb1b3eb18e6e3efbea772489854fa2e59fc5
Justin Domingue <justin.domingue@hotmail.com>
2012-06-13 22:47:06 -0400

    Improved the title handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def title
+    base_title = 'Motion Bombs'
+    if @title.empty?
+      base_title
+    else
+      "#{@title} | #{base_title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Motionbombs::Application.routes.draw do
+  
+  root :to => 'pages#home'
+  
+  get "pages/about"
+  get "pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
