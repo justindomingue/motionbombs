bd6dc8230175a4d9b37bfd8ff49c441b49de7ae4
Justin Domingue <justin.domingue@hotmail.com>
2012-06-13 22:03:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motionbombs::Application.config.secret_token = 'e00af2339802e185981935df2681c50b181d423d7757ce6e828a807dbabb6d61b0ebf0c5fabeec04301e70a41c0a5c73eb33222556da563675ffe694fa98937c'
