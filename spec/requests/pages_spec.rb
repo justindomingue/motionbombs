c1ae1d1c369b26d6e49f01c5e328d2c709b07225
Justin Domingue <justin.domingue@hotmail.com>
2012-06-15 15:20:18 -0400

    Added Pages Controller

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "Pages" do
+  describe "GET /" do
+    it "has the right title" do
+      visit '/'
+      page.should have_selector('title', :text => 'Home | Motion Bombs')
+    end
+  end
+  
+  describe "GET /pages/about" do
+    it "has the right title" do
+      visit '/pages/about'
+      page.should have_selector('title', :text => 'About | Motion Bombs')
+    end
+  end
+  
+  describe "GET /pages/help" do
+    it "has the right title" do
+      visit '/pages/help'
+      page.should have_selector('title', :text => 'Help | Motion Bombs')
+    end
+  end
+end
