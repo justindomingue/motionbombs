1ac5aa0f727c08693f3a4baed033f1e782c0b57e
Justin Domingue <justin.domingue@hotmail.com>
2012-07-27 15:46:47 -0400

    Removed counter cache for migrating on heroku

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,8 @@ class Like < ActiveRecord::Base
   attr_accessible :user_id, :video_id
   
   belongs_to :user
-  belongs_to :video, :counter_cache => true
+  #belongs_to :video, :counter_cache => true
+  belongs_to :video
   
   validates_presence_of :user_id, :video_id
 
