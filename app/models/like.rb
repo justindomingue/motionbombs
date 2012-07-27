8a749c2b392762643cf965a4cfc5470fad085995
Justin Domingue <justin.domingue@hotmail.com>
2012-07-27 15:53:59 -0400

    Removed counter cache for migrating on heroku

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,7 @@
 class Like < ActiveRecord::Base
   attr_accessible :user_id, :video_id
   
-  belongs_to :user
-  #belongs_to :video, :counter_cache => true
+  belongs_to :video, :counter_cache => true
   belongs_to :video
   
   validates_presence_of :user_id, :video_id
--- app/models/video_comment.rb
+++ app/models/video_comment.rb
@@ -4,7 +4,6 @@ class VideoComment < ActiveRecord::Base
   validates_presence_of :user_id, :video_id, :content
   validates :content, :length => { :maximum => 200, :too_long => "%{count} characters is the maximum allowed" }
   
-  belongs_to :video
-  #belongs_to :video, :counter_cache => true
+  belongs_to :video, :counter_cache => true
   belongs_to :user
 end
